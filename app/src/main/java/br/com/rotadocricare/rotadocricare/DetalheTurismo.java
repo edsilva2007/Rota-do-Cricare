c8615be79e830a4b1317d224560dfe8d9954ae95
Edsilva2007 <edsilva2007@gmail.com>
2019-09-18 08:03:09 -0300

    Inserção do Modelo da Aplicação

--- app/src/main/java/br/com/rotadocricare/rotadocricare/DetalheTurismo.java
+++ app/src/main/java/br/com/rotadocricare/rotadocricare/DetalheTurismo.java
@@ -1,16 +1,4 @@
 package br.com.rotadocricare.rotadocricare;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-import br.com.rotadocricare.rotadocricare.R;
-
-public class DetalheTurismo extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detalhe_turismo);
-    }
+public class DetalheTurismo {
 }
